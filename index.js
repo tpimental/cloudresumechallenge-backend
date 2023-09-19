4e8d0fdafc0d17f8aef13586bf5d98f60f1f61d0
Tyler Pimental <tylerpimental@gmail.com>
2023-09-18 22:42:33 -0700

    test
    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req, data) {
 context.bindings.outputDocument = data[0];
-context.bindings.outputDocument.count += 2;
+context.bindings.outputDocument.count += 3;
 context.res = {
 body: data[0].count
 };
