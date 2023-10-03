10021d67452175698abe8fd3354948c3a2b3e6a1
Tyler Pimental <tylerpimental@gmail.com>
2023-10-03 00:20:04 -0700

    updated functions

--- increment-counter-func/index.js
+++ increment-counter-func/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req, data) {
     context.bindings.outputDocument = data[0];
-    context.bindings.outputDocument.count += 3;
+    context.bindings.outputDocument.count += 1;
     context.res = {
     body: data[0].count
     };
